b667d9b2d520ce8324daa7069e7851eff492f66a
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-30 11:25:44 -0400

    finally working

--- main-app.js
+++ main-app.js
@@ -1,14 +1,21 @@
-const result = window.prompt('Please, type in some numbers!')
-const arrNum = result.split(' ')
-operation = result[0]  
-num1 = Number(arrNum[1])
-num2 = Number(arrNum[2])
-// Array Deconstruction
-// const [operation, num1, num2] = result
-const resolution = calculate(operation, num1, num2);
- 
-console.log(resolution)
+let weightInputs = window.prompt('Please, type in your weight and units')
+// const arrNum = result.split(' ')
+// operation = result[0]  
+// num1 = Number(arrNum[1])
+// num2 = Number(arrNum[2])
+// // Array Deconstruction
+// // const [operation, num1, num2] = result
+// const resolution = calculate(operation, num1, num2);
+
+// console.log(resolution)
+let arrNum = weightInputs.split(' ')
+const weight = arrNum[0]
+const unit = arrNum[1]
+// const space = arrNum[1]
+// const [num1,num2] = result
+const result = weightConverter(weight, unit) + '.' + ' ' + advice(weight, unit) + '!'
+
 const printAnswer = document.querySelector('#result');
-printAnswer.innerText = resolution
+printAnswer.innerText = result
 
 //document.querySelector('#result').innerText = calculate(operation, num1, num2)
--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-const userInput1 = process.argv[2];
 
-const userInput2 = process.argv[3];
-
-if (userInput2 === 'kg') {
-    toPounds(userInput1);
-} else if (userInput2==='lbs') {
-    toKg(userInput1);
-} else {
-    return console.log('check your units please');
-}
-
-function toKg (pounds) {
-    const kg = 0.453592 * pounds;
-    console.log(kg +'kg');
-}
-
-function toPounds(kg) {
-    const pounds = 2.20462 * kg
-    console.log(pounds +'lbs');
-}
+// if (userInput2 === 'kg') {
+//     toPounds(userInput1);
+// } else if (userInput2==='lbs') {
+//     toKg(userInput1);
+// }
+// } else {
+//     console.log('check your units please');
+// }
 
 
-
-
-function advice(userInput1) {
- 
-    
-    
-    if (userInput1>=100) {
-        return "cut down calories and get yourself to the gym son!"
-    } else if (userInput1>=90) {
-        return "eat the right foods and work out a little every week"
-    } else if (userInput1>=80) {
-        return "you're doing alright don't worry about it. You could potentially up your activity level"
-    } else if (userInput1>=70) {
-        return "you're perfect"
-    } else if (userInput1>=60) {
-        return "eat a little more"
+function weightConverter(weight, unit) {
+    if (unit==='kg' || unit==='kgs' || unit==='kilograms') {
+      return weight * 2.20462 + 'lbs'
+    } else if (unit==='lbs' || unit==='pounds'){
+      return weight * 0.453592 + 'kgs'
     } else {
-        return "up the calories and maintain a healthy diet"
+      return "check your units please"
+    }
+  }
+
+//   advice(100) -> 'You\'re doing great!'
+
+// '100 kg' -> 'You weigh 180 pounds. You\'re doing great!'
+// '180 pounds' -> 'You weigh 100 kgs. You\'re doing great!'
+
+function advice(weight, unit) {
+
+    // Check kg before or after conversion.
+    if ((weight>=100 && (unit === 'kg' || unit === 'kgs' || unit === 'kilograms')) || (weight>=220 && (unit === 'lbs' || unit === 'pounds'))) {
+        return "Cut down calories and get yourself to the gym son"
+    } else if ((weight>=90 && (unit === 'kg' || unit === 'kgs' || unit === 'kilograms')) || (weight>=198 && (unit === 'lbs' || unit === 'pounds'))) {
+        return "Eat the right foods and work out a little every week"
+    } else if ((weight>=80 && (unit === 'kg' || unit === 'kgs' || unit === 'kilograms')) || (weight>=176 && (unit === 'lbs' || unit === 'pounds'))) {
+        return "You're doing alright don't worry about it. You could potentially up your activity level"
+    } else if ((weight>=70 && (unit === 'kg' || unit === 'kgs' || unit === 'kilograms')) || (weight>=154 && (unit === 'lbs' || unit === 'pounds'))) {
+        return "You're perfect"
+    } else if ((weight>=60 && (unit === 'kg' || unit === 'kgs' || unit === 'kilograms')) || (weight>=132 && (unit === 'lbs' || unit === 'pounds'))) {
+        return "Eat a little more"
+    } else {
+        return "Up the calories and maintain a healthy diet"
     }
 }
 
-advice(userInput1);
+// advice(userInput1);
 
-console.log(advice(userInput1));
+// console.log(advice(userInput1));
